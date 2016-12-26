f2aca97660cfb31128097e15e86f9e8f96310589
nmadd <nate@fratmusic.com>
2016-12-26 14:49:46 -0500

    add particle js and basic config setup

--- app/App.js
+++ app/App.js
@@ -8,29 +8,70 @@ import {Provider} from 'react-redux';
 import store from './store';
 import {setCurrentInfo} from './actions';
 import {compose} from 'redux';
+import Particles from 'react-particles-js';
 
+const styles = {
+  particles : {
+    position: 'relative'
+  },
+  content: {
+    position: 'absolute',
+    top: 0,
+    backgroundColor: 'rgba(0,0,0,0)'
+  }
+}
+
+const particleParams = {
+        particles: {
+          number: {
+            value: 50
+          },
+          color: {
+            value: '#ff00ff'
+          },
+          shape: {
+            type: 'circle'
+          },
+          size: {
+            value: 3
+          },
+          line_linked: {
+            enable_auto: true,
+            distance: 150,
+            color: '#000000'
+          },
+          move: {
+            direction: 'right',
+            out_mode: 'out',
+            speed: 1
+          }
+        }
+}
 
 const App = React.createClass({
   render() {
     return (
-      <div>
-        <Timeline />
-        {this.props.children}
+      <div >
+        <Particles style={styles.particles} params={particleParams} />
+        <div style={styles.content}>
+          <Timeline/>
+          {this.props.children}
+        </div>
       </div>
     )
   }
 })
 
-const setInfoOnEnter = (nextState) => {store.dispatch(setCurrentInfo(nextState.params.num))}
+const setInfoOnEnter = (nextState) => {
+  store.dispatch(setCurrentInfo(nextState.params.num))
+}
 const routeEnterHandler = compose(setInfoOnEnter);
 
 ReactDOM.render(
-  <Provider store={store} >
-    <Router history={browserHistory}>
-      <Route path='/' component={App}>
-        <Route path='info/:num' component={Info} onEnter={routeEnterHandler}/>
-      </Route>
-    </Router>
-  </Provider>,
-  document.getElementById('root')
-)
+  <Provider store={store}>
+  <Router history={browserHistory}>
+    <Route path='/' component={App}>
+      <Route path='info/:num' component={Info} onEnter={routeEnterHandler}/>
+    </Route>
+  </Router>
+</Provider>, document.getElementById('root'))
