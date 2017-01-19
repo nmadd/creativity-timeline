725488dabab7e658d7e74f4b2468bb84a9bfd828
nmadd <nate@fratmusic.com>
2017-01-19 15:49:03 -0500

    fix svg issues on firefox and positioning issues all over

--- app/circle/Circle.jsx
+++ app/circle/Circle.jsx
@@ -2,9 +2,22 @@ import React from 'react';
 import {Link} from 'react-router';
 import store from '../store';
 
+const circleProps = {
+  transition: 'r .5s ease',
+  fill: 'white',
+  stroke: 'lightskyblue',
+  strokeWidth: 2,
+  r: 15
+}
+
+const activeCircleProps = {
+  fill: 'lightskyblue',
+  r: 20
+}
+
 const Circle = props => (
-  <Link to={`/info/${props.post}`}>
-    <circle className={props.post === store.getState().activePath ? 'circle active-circle' : 'circle'}  cx={props.cx} cy={props.cy}  stroke="10"  />
+  <Link to={`/info/${props.post}`} className='circle-link'>
+    <circle stroke='lightskyblue' strokeWidth='2' r={props.post === store.getState().activePath ? activeCircleProps.r : circleProps.r}  fill={props.post === store.getState().activePath ?  activeCircleProps.fill : circleProps.fill} className={props.post === store.getState().activePath ? 'circle active-circle' : 'circle'}  cx={props.cx} cy={props.cy}  stroke="10"  />
   </Link>
 );
 
