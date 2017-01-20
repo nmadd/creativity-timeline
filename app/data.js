7749de1d533d0a283042f3aacbfd3fdc68630d69
nmadd <nate@fratmusic.com>
2017-01-20 10:51:20 -0500

    keep updating data

--- app/data.js
+++ app/data.js
@@ -2,7 +2,7 @@ export default [
   {
     text: [`In late 2007 I started hosting a radio show at my campus radio station. I was excited because they were giving me a lot of creative freedom. You could basically play whatever you liked, as long as you had some sort of theme, and as long as you didn't play anything you'd normally hear on the radio.`,
 
-    `I perhaps took that a little too literally and decided to do a show called Campfire Singalongs, which only played experimental electronic music that didn't have any words. I also lived right next to the radio station, so I agreed to take any time slot available. I ended up with the primetime slot of Wednesday nights, from 11PM - 1AM.`,
+    `I perhaps took that a little too literally and decided to do a show called Campfire Singalongs, which only played experimental electronic music that didn't have any words. I also lived right next to the radio station, so I agreed to take any time slot available. I ended up with the primetime slot of Wednesday nights, from 11 PM - 1 AM.`,
 
   `Needless to say, I did not have many listeners. But I loved the experience of putting together a show, creating a connecting theme and story, and putting it out there for anybody who cared.`],
     year: 2007,
@@ -11,7 +11,7 @@ export default [
   {
     text: [`That experience began to pay off when, two years later, I suddenly became an entrepreneur.`,
 
-    `Midway through my senior year at University of Virginia, I started a music website called 'FratMusic' with three friends. Although none of us were actually in a fraternity, we noticed that the fraternities around our school paid local DJs to play at their parties. But the DJs usually just played standard, top-40 hits. So we thought there might be an opportunity to create a better solution. `],
+    `Midway through my senior year at the University of Virginia, I started a music website called 'FratMusic' with three friends. Although none of us were actually in a fraternity, we noticed that the fraternities around our school paid local DJs to play at their parties. But the DJs usually just played standard, top-40 hits. So we thought there might be an opportunity to create a better solution. `],
     year: 2009,
     path: 'post2'
   },
@@ -25,9 +25,9 @@ export default [
     path: 'post3'
   },
   {
-    text: [`For the first few years, I led product, branding, and content for our website. But then, with the fast rise of the iPhone, we suddenly had a whole new market: smart phone apps.`,
+    text: [`For the first few years, I led product, branding, and content for our website. But then, with the fast rise of the iPhone, we suddenly had a whole new market: smartphone apps.`,
 
-    `Over the course of about a year and a half, I led teams of designers and developers from initial concepts through finished products for both iOS and Android apps. Since we already had an established web presence, we decided to sell our apps, instead of release them for free. We ended up selling over 100,000 of them, which financed us over the next couple years.
+    `Over the course of about a year and a half, I led teams of designers and developers from initial concepts through finished products for both iOS and Android apps. Since we already had an established web presence, we decided to sell our apps, instead of releasing them for free. We ended up selling over 100,000 of them, which financed us over the next couple years.
     `],
     year: 2012,
     path: 'post4'
@@ -39,15 +39,15 @@ export default [
   },
   {
     text: [`
-    For the few years prior, while I had been running FratMusic, I had also been slowly exploring how to code. Coding combined two of my favorite things: writing and solving puzzles. So in 2015, I made the difficult decision to move on from FratMusic and decided to take some time to explore my newfound interest in coding, and try to find something with a mission that I really cared about.`],
+    For the few years prior, while I had been running FratMusic, I had also been slowly exploring how to code. Coding combined two of my favorite things: writing and solving puzzles. So in 2015, I made the difficult decision to move on from FratMusic and decided to take some time to explore my newfound interest in coding and try to find something with a mission that I really cared about.`],
     year: 2015,
     path: 'post6'
   },
   {
     text: [`After attending a four-month coding school in Manhattan, I stumbled upon a small, non-profit coding school in Queens, NY that was focusing on teaching coding to underserved populations.
       I was immediately drawn in by their goal.`,
-    ` Soon after, I began working as an instructor at Coalition for Queens, where I lead daily lessons in web developmenet and computer science for a diverse group of students from a wide variety of backgrounds.
-    It's been incrediby fulfilling to get to know the students on a personal level and to watch them grow from week to week and month to month.
+    ` Soon after, I began working as an instructor at Coalition for Queens, where I lead daily lessons in web development and computer science for a diverse group of students from a wide variety of backgrounds.
+    It's been incredibly fulfilling to get to know the students on a personal level and to watch them grow from week to week and month to month.
     I've also gained experience in everything from building the overall arch of our curriculum,
     to managing the dynamics of a classroom,
     to learning how to build a non-profit. `],
@@ -59,14 +59,13 @@ export default [
 
     Going forward, I would love to continue to explore the intersection of technology and business.
     Ultimately, I want to be a CTO or founder of a company that is working to solve one of the many big problems in fields like education, health, and technology.
-    Along the way I would like to continue gain diverse work experience and explore roles in technical product managegement and consulting.`,
+    Along the way, I would like to continue to gain diverse work experience and explore roles in technical product management and consulting.`,
 
-    `Throughout my experience, I have come to realize that solving the hardest problems always comes down to having a well organized team of great people.
-    Even the best technology is ineffective without a well managed group of people behind it.
-    I believe that, moving forward, it will be crucial to have a deep understanding of both technology and human dynamics, and how they interact.
+    `Throughout my experience, I have come to realize that even the best technology is ineffective without a well-organized team of great people behind it.
+    I believe that moving forward, it will be crucial to have a deep understanding of both technology and human dynamics, and how they interact.
     I'm interested in pursuing an MBA to learn the science of leading people, and the art of balancing the best of technology and humanity.`,
 
-    `I am drawn to Cornell Tech because of its hands on, immersive learning environment, which I believe is the best way to learn.
+    `I am drawn to Cornell Tech because of its hands-on, immersive learning environment, which I believe is the best way to learn.
     I am also drawn to the inherently entrepreneurial nature of the program, and the interdisciplinary mix of MBA and Computer Science students.
     I would love the opportunity to continue my journey at Cornell Tech.`],
     year: 2017,
