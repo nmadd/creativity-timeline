44155cf7a70863b69521e99da19a52ab2c25b7d4
nmadd <nate@fratmusic.com>
2017-01-20 10:33:40 -0500

    closes #43

--- app/data.js
+++ app/data.js
@@ -1,15 +1,15 @@
 export default [
   {
-    text: [`In late 2007 I started hosting a radio show at my campus radio station. I was excited because they were giving me a lot of creative freedom. The rule at the station was you could basically play whatever you liked, as long as you had some sort of theme, and as long as you didn't play anything you'd normally hear on the radio.`,
+    text: [`In late 2007 I started hosting a radio show at my campus radio station. I was excited because they were giving me a lot of creative freedom. You could basically play whatever you liked, as long as you had some sort of theme, and as long as you didn't play anything you'd normally hear on the radio.`,
 
-    `I perhaps took that a little too literally. I decided to do a show called Campfire Singalongs, which only played experimental electronic music that didn't have any words. I also lived right next to the radio station, so I agreed to take any time slot available. I ended up with the primetime slot of Wednesday nights, from 11PM - 1AM.`,
+    `I perhaps took that a little too literally and decided to do a show called Campfire Singalongs, which only played experimental electronic music that didn't have any words. I also lived right next to the radio station, so I agreed to take any time slot available. I ended up with the primetime slot of Wednesday nights, from 11PM - 1AM.`,
 
-  `Needless to say, I did not have many listeners. But I loved the experience of putting together a show, creating some type of connecting theme and story, and putting it out there for anybody who cared.`],
+  `Needless to say, I did not have many listeners. But I loved the experience of putting together a show, creating a connecting theme and story, and putting it out there for anybody who cared.`],
     year: 2007,
     path: 'post1'
   },
   {
-    text: [`That experience paid off when, two years later, I suddenly became an entrepreneur.`,
+    text: [`That experience began to pay off when, two years later, I suddenly became an entrepreneur.`,
 
     `Midway through my senior year at University of Virginia, I started a music website called 'FratMusic' with three friends. Although none of us were actually in a fraternity, we noticed that the fraternities around our school paid local DJs to play at their parties. But the DJs usually just played standard, top-40 hits. So we thought there might be an opportunity to create a better solution. `],
     year: 2009,
@@ -18,14 +18,14 @@ export default [
   {
     text:[
     `
-    FratMusic began as a simple playlist. I would listen to the music that was being played on campus, scour the internet for good remixes and mashups of those songs, and add them to FratMusic.`,
+    FratMusic began as a simple playlist. I would listen to the music that was being played on campus, scour the internet for good remixes and mashups of those songs, and add them to the site.`,
     `After a few months, we added a new feature that allowed any college student to submit a playlist. The next thing we knew, our site unexpectedly started growing and turning into a burgeoning community of DJs and music lovers. By the time our college graduation approached, FratMusic had reached all fifty states and was getting over 80,000 unique users a day.
     `],
     year: 2010,
     path: 'post3'
   },
   {
-    text: [`For the first few years, I led product, branding, and content for FratMusic. Then, with the incredibly fast rise of the iPhone, we found a great new market: almost every college student suddenly had a smart phone.`,
+    text: [`For the first few years, I led product, branding, and content for our website. But then, with the fast rise of the iPhone, we suddenly had a whole new market: smart phone apps.`,
 
     `Over the course of about a year and a half, I led teams of designers and developers from initial concepts through finished products for both iOS and Android apps. Since we already had an established web presence, we decided to sell our apps, instead of release them for free. We ended up selling over 100,000 of them, which financed us over the next couple years.
     `],
@@ -33,20 +33,24 @@ export default [
     path: 'post4'
   },
   {
-    text: [`But despite our initial success, expanding past our relatively niche market was hard. When we started off the music market was pretty new, but by 2014 it was almost completely taken over by bigger companies. Additionally, by that time I was over five years out of college, and I was having a harder and harder time connecting with our companies brand and mission.`],
+    text: [`But despite our initial success, expanding past our relatively niche market was hard. When we started off the digital music industry was pretty new, but by 2014 it was almost completely taken over by bigger companies. Additionally, by that time I was over five years out of college, and I was having a harder and harder time connecting with our companies brand and mission.`],
     year: 2014,
     path: 'post5'
   },
   {
     text: [`
-    For the few years prior, while I had been running FratMusic, I had also been slowly exploring how to code. Coding combined two of my favorite things: writing and solving puzzles. So in 2015, I made the difficult decision to move on from FratMusic and decided to take some time to explore my newfound interest in coding. And this time around, I also wanted to do something that had a mission that I really cared about.`],
+    For the few years prior, while I had been running FratMusic, I had also been slowly exploring how to code. Coding combined two of my favorite things: writing and solving puzzles. So in 2015, I made the difficult decision to move on from FratMusic and decided to take some time to explore my newfound interest in coding, and try to find something with a mission that I really cared about.`],
     year: 2015,
     path: 'post6'
   },
   {
     text: [`After attending a four-month coding school in Manhattan, I stumbled upon a small, non-profit coding school in Queens, NY that was focusing on teaching coding to underserved populations.
-      I was immediately drawn in by their mission.`,
-    ` Soon after, I began working as a web development instructor at Coalition for Queens, where I've gained experience in everything from building the overall arch of our curriculum, to managing the dynamics of a classroom, to learning how to build a non-profit. `],
+      I was immediately drawn in by their goal.`,
+    ` Soon after, I began working as an instructor at Coalition for Queens, where I lead daily lessons in web developmenet and computer science for a diverse group of students from a wide variety of backgrounds.
+    It's been incrediby fulfilling to get to know the students on a personal level and to watch them grow from week to week and month to month.
+    I've also gained experience in everything from building the overall arch of our curriculum,
+    to managing the dynamics of a classroom,
+    to learning how to build a non-profit. `],
     year: 2016,
     path: 'post7'
   },
@@ -74,7 +78,7 @@ const text8 = [`
 
 Going forward, I would love to continue to explore the intersection of technology and business.
 Ultimately, I want to be a CTO or founder of a company that is working to solve one of the many big problems in fields like education, health, and technology.
-Along the way I would like to continue gain diverse work experience and explore roles in technical product managegement and consulting.`,
+Along the way I would also like to continue to gain diverse work experience and explore roles in technical product management and consulting.`,
 
 `Throughout my experience, I have come to realize that solving the hardest problems always comes down to having a well organized team of great people.
 Even the best technology is ineffective without a well managed group of people behind it.
